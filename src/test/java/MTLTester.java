6fe3cdbe5176e1c782ff177c8dd8e758c4ace978
Kevin Baldor <kevin.baldor@gmail.com>
2015-08-04 15:54:17 -0500

    Added tests for Previously and improved Since test

--- src/test/java/MTLTester.java
+++ src/test/java/MTLTester.java
@@ -1,11 +1,9 @@
 import junit.framework.TestCase;
 import lithium.*;
 import lithium.mtl.Transition;
-import lithium.mtl.MTL.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 
@@ -22,107 +20,128 @@ public class MTLTester extends TestCase {
 
         Cell<Transition> n = lithium.mtl.MTL.not(p);
         ArrayList<Transition.Value> out = new ArrayList<>();
-        Listener l = n.listen(x -> { out.add(x.value());});
+        Listener l = n.listen(x -> out.add(x.value()));
         p.send(Transition.TF());
-        p.send(Transition.FF());
+        p.send(Transition.TT());
         l.unlisten();
-        assertEquals(Arrays.asList(Transition.Value.TT, Transition.Value.FT, Transition.Value.TT), out);
+        assertEquals(Arrays.asList(Transition.Value.TT, Transition.Value.FT, Transition.Value.FF), out);
     }
-
 
     public void testAnd()
-    {
-        CellSink<Transition> p = new CellSink<>(Transition.FF());
-        CellSink<Transition> q = new CellSink<>(Transition.TT());
-        CellSink<Transition> r = new CellSink<>(Transition.TT());
+        {
+            CellSink<Transition> p = new CellSink<>(Transition.FF());
+            CellSink<Transition> q = new CellSink<>(Transition.TT());
+            CellSink<Transition> r = new CellSink<>(Transition.TT());
 
-        Cell<Transition> a = lithium.mtl.MTL.and(lithium.mtl.MTL.and(p, q),r);
-        ArrayList<Transition.Value> out = new ArrayList<>();
-        Listener l = a.updates().listen(x -> { out.add(x.value());});
-        Transaction.run(new Handler<Transaction>() {
-            @Override
-            public void run(Transaction transaction) {
-                p.send(Transition.FT());
-                q.send(Transition.TF());
-            }
-        });
-        Transaction.run(new Handler<Transaction>() {
-            @Override
-            public void run(Transaction transaction) {
-                p.send(Transition.FT());
-                q.send(Transition.TT());
-            }
-        });
-        r.send(Transition.TF());
-        l.unlisten();
-        assertEquals(Arrays.asList(Transition.Value.FF, Transition.Value.FT, Transition.Value.TF), out);
-    }
+            Cell<Transition> a = lithium.mtl.MTL.and(lithium.mtl.MTL.and(p, q),r);
+            ArrayList<Transition.Value> out = new ArrayList<>();
+            Listener l = a.listen(x -> out.add(x.value()));
+            Transaction.run((t) -> {
+                    p.send(Transition.FT());
+                    q.send(Transition.TF());
+            });
+            Transaction.run((t) -> {
+                    p.send(Transition.FT());
+                    q.send(Transition.TT());
+            });
+            r.send(Transition.TF());
+            l.unlisten();
+            assertEquals(Arrays.asList(Transition.Value.FF, Transition.Value.FT, Transition.Value.TF), out);
+        }
 
-    public void testOr()
-    {
-        CellSink<Transition> p = new CellSink<>(Transition.FF());
-        CellSink<Transition> q = new CellSink<>(Transition.TT());
-        CellSink<Transition> r = new CellSink<>(Transition.FF());
+        public void testOr()
+        {
+            CellSink<Transition> p = new CellSink<>(Transition.FF());
+            CellSink<Transition> q = new CellSink<>(Transition.TT());
+            CellSink<Transition> r = new CellSink<>(Transition.FF());
 
-        Cell<Transition> a = lithium.mtl.MTL.or(lithium.mtl.MTL.or(p, q), r);
-        ArrayList<Transition.Value> out = new ArrayList<>();
-        Listener l = a.updates().listen(x -> { out.add(x.value());});
-        Transaction.run(new Handler<Transaction>() {
-            @Override
-            public void run(Transaction transaction) {
+            Cell<Transition> a = lithium.mtl.MTL.or(lithium.mtl.MTL.or(p, q), r);
+            ArrayList<Transition.Value> out = new ArrayList<>();
+            Listener l = a.updates().listen(x -> out.add(x.value()));
+            Transaction.run(t ->  {
                 p.send(Transition.TF());
                 q.send(Transition.TF());
-            }
-        });
-        Transaction.run(new Handler<Transaction>() {
-            @Override
-            public void run(Transaction transaction) {
+
+            });
+            Transaction.run(t -> {
                 p.send(Transition.FT());
                 q.send(Transition.TT());
-            }
-        });
-        r.send(Transition.TF());
-        l.unlisten();
-        assertEquals(Arrays.asList(Transition.Value.TF, Transition.Value.TT, Transition.Value.TT), out);
-    }
 
-    public void testSince()
-    {
-        CellSink<Transition> p = new CellSink<>(Transition.FF());
-        CellSink<Transition> q = new CellSink<>(Transition.TT());
+            });
+            r.send(Transition.TF());
+            l.unlisten();
+            assertEquals(Arrays.asList(Transition.Value.TF, Transition.Value.TT), out);
+        }
 
-        Cell<Transition> s = lithium.mtl.MTL.since(p,q);
+        public void testSince()
+        {
+            CellSink<Transition> p = new CellSink<>(Transition.TT());
+            CellSink<Transition> q = new CellSink<>(Transition.FF());
 
-        ArrayList<Transition.Value> out = new ArrayList<>();
-//        Listener l = s.updates().listen(x -> { out.add(x.value());});
-        Listener l = s.listen(x -> {
-            out.add(x.value());
-        });
-        Transaction.run(new Handler<Transaction>() {
-            @Override
-            public void run(Transaction transaction) {
+            Cell<Transition> s = lithium.mtl.MTL.since(p,q);
+
+            ArrayList<Transition.Value> out = new ArrayList<>();
+            Listener l = s.listen(x -> out.add(x.value()));
+            Transaction.run(t -> {
                 p.send(Transition.FT());
                 q.send(Transition.TF());
-            }
-        });
-        p.send(Transition.FT());
-        p.send(Transition.TF());
-        q.send(Transition.TF());
-        q.send(Transition.FT());
-        p.send(Transition.TT());
-        q.send(Transition.FF());
+            });
+            p.send(Transition.FT());
+            p.send(Transition.TF());
+            q.send(Transition.TF());
+            q.send(Transition.FT());
+            p.send(Transition.TT());
+            q.send(Transition.FF());
+            l.unlisten();
+            assertEquals(Arrays.asList(
+                    Transition.Value.FF, // initial value
+                    Transition.Value.TT,
+                    Transition.Value.FF,
+                    Transition.Value.TF,
+                    Transition.Value.FT), out);
+        }
+
+    public void testPreviously()
+    {
+        CellSink<Transition> p = new CellSink<>(Transition.FF());
+
+        Cell<Transition> pr = lithium.mtl.MTL.previously_cc(p,1000);
+        ArrayList<Transition.Value> out = new ArrayList<>();
+        Listener l = pr.listen(x -> out.add(x.value()));
+        try {
+            p.send(Transition.TF());
+            Thread.sleep(500);
+            p.send(Transition.TF());
+            Thread.sleep(500);
+            p.send(Transition.FT());
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         l.unlisten();
-        assertEquals(Arrays.asList(
-                Transition.Value.TT, // initial value
-                Transition.Value.TT,
-                Transition.Value.FF,
-                Transition.Value.FF,
-                Transition.Value.TF,
-                Transition.Value.FT,
-                Transition.Value.TT,
-                Transition.Value.TT), out);
+        assertEquals(Arrays.asList(Transition.Value.FF, Transition.Value.TT), out);
     }
 
+    public void testPreviously2()
+    {
+        CellSink<Transition> p = new CellSink<>(Transition.FF());
+
+        Cell<Transition> pr = lithium.mtl.MTL.previously_cc(p,1000);
+        ArrayList<Transition.Value> out = new ArrayList<>();
+        Listener l = pr.listen(x -> out.add(x.value()));
+        try {
+            p.send(Transition.TF());
+            Thread.sleep(500);
+            p.send(Transition.TF());
+            Thread.sleep(500);
+            p.send(Transition.TF());
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        l.unlisten();
+        assertEquals(Arrays.asList(Transition.Value.FF, Transition.Value.TT, Transition.Value.TF), out);
+    }
 
 }
 
